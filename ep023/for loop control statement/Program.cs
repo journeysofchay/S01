b3d3ea51ba5c31f17caf0e65ad1daaceefcf1d29
Chaychan-J <122243369+journeysofchay@users.noreply.github.com>
2023-01-11 22:12:34 +0700

    Create Program.cs

---
+++ ep023/for loop control statement/Program.cs
@@ -0,0 +1,25 @@
+﻿
+float display_result_sec = 3.5f;
+int display_result_millisec = (int)(display_result_sec*1000);
+
+
+
+
+Console.Write("\n\n\n");
+
+
+
+
+
+for(   int i=1   ;   i < 60000   ;  i*=2    )
+{
+    Console.Write(i+" ");
+}
+
+
+
+Thread.Sleep(display_result_millisec);
+return;
+
+
+
