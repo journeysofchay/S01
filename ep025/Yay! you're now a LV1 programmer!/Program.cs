ad0ae0e17ccf0459725797cd83d3cb5f5c472aeb
Chaychan-J <122243369+journeysofchay@users.noreply.github.com>
2023-01-25 23:32:01 +0700

    Create Program.cs

---
+++ ep025/Yay! you're now a LV1 programmer!/Program.cs
@@ -0,0 +1,36 @@
+﻿float display_result_sec = 7f;
+int display_result_millisec = (int)(display_result_sec*1000);
+
+
+
+
+
+
+
+
+
+
+
+
+string eye_on_his_left="-";
+string eye_on_his_right="-";
+string mouth="[]";
+string head_n_face;
+
+// render
+head_n_face=
+@"  \\"+ "\n" +
+@"  \\\\\"+"\n" +
+@" / \\  \\"+"\n" +
+@"[| "+eye_on_his_right+"  "+eye_on_his_left+" |]"+"\n" +
+@"\| "+mouth+"   |/"+"\n"+
+@"  \    /"+"\n"+
+@"    --"+"\n";
+
+
+Console.WriteLine(head_n_face);
+
+
+
+
+Thread.Sleep(display_result_millisec);
